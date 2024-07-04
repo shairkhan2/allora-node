47a755848a512b152801c3c5ed1f743cf941db47
shairkhan2 <130557140+shairkhan2@users.noreply.github.com>
2024-07-04 12:14:33 +0530

    Update allora-node-simple.sh

--- allora-node-simple.sh
+++ allora-node-simple.sh
@@ -1,4 +1,6 @@
-sudo apt update && sudo apt upgrade -y && \
+sudo apt update && \
+sudo apt install -y dos2unix && \
+sudo apt upgrade -y && \
 sudo apt install -y ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev curl git wget make jq build-essential pkg-config lsb-release libssl-dev libreadline-dev libffi-dev gcc screen unzip lz4 && \
 sudo apt install -y python3 python3-pip && \
 python3 --version && \
@@ -28,3 +30,4 @@ git clone https://github.com/allora-network/allora-chain.git && \
 cd allora-chain && make all && \
 allorad version && \
 allorad keys add testkey
+
